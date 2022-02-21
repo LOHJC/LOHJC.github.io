4e093882ff5eaff8499e7d879feb511613ef2a75
LOHJC <81831799+LOHJC@users.noreply.github.com>
2022-02-21 23:09:10 +0800

    Add files via upload
    change design to make sure has mobile view

--- index.js
+++ index.js
@@ -1,106 +1,5 @@
 "use strict"
 
-function setHeaderFooterFontSize()
-{
-	//get header and footer
-	let header = document.querySelector("header");
-	let footer = document.querySelector("footer");
-
-	//get header and footer height and width
-	let header_height = header.offsetHeight;
-	let header_width = header.offsetWidth;
-	let footer_height = footer.offsetHeight;
-	let footer_width = footer.offsetWidth;
-
-	//set header text and footer text fit
-	let header_scale = 7;
-	let footer_scale = 2.5;
-	
-	if (header_width > header_height)
-		header.style.fontSize = header_scale+"vh";
-	else
-		header.style.fontSize = header_scale+"vw";
-	
-	if (footer_width > footer_height)
-		footer.style.fontSize = footer_scale+"vh";
-	else
-		footer.style.fontSize = footer_scale+"vw";
-}
-
-function setNavBarFontSize()
-{
-	let nav_buttons = document.querySelector("nav").querySelector("table").querySelector("tr").querySelectorAll("td");
-	let nav_buttons_len = nav_buttons.length;
-	
-	let text_scale = 3;
-	
-	for (let i=0; i<nav_buttons_len; i++)
-	{
-		let nav_button = nav_buttons[i].querySelector("button");
-		let nav_button_height = nav_button.offsetHeight;
-		let nav_button_width = nav_button.offsetWidth;
-		
-		if (nav_button_width > nav_button_height)
-			nav_button.style.fontSize = text_scale+"vh";
-		else
-			nav_button.style.fontSize = text_scale+"vw";
-	}
-}
-
-function setContentFontSize()
-{
-	//set title
-	let title_div = document.getElementById("content_title")
-	let title_div_height = title_div.offsetHeight;
-	let title_div_width = title_div.offsetWidth;
-	
-	let title_scale = 5.5;
-	
-	if (title_div_width > title_div_height)
-		title_div.style.fontSize = title_scale+"vh";
-	else
-		title_div.style.fontSize = title_scale+"vw";
-	
-	//set description
-	let description_div = document.getElementById("content_description")
-	let description_div_height = description_div.offsetHeight;
-	let description_div_width = description_div.offsetWidth;
-	
-	let description_scale = 3.5;
-	
-	if (description_div_width > description_div_height)
-	{
-		description_div.style.fontSize = description_scale+"vh";
-		description_div.style.padding = "0 " + description_scale+"vh";
-	}
-	else
-	{
-		description_div.style.fontSize = description_scale+"vw";
-		description_div.style.padding = "0 " + description_scale+"vw";
-	}
-}
-
-function setCardDescriptionFontSize()
-{
-	let card_description = document.getElementById("card_description");
-	
-	if (card_description)
-	{
-		let card_description_height = card_description.offsetHeight;
-		let card_description_width = card_description.offsetWidth;
-		
-		let scale=3;
-		
-		if (card_description_width > card_description_height)
-		{
-			card_description.style.fontSize = scale+"vh";
-		}
-		else
-		{
-			card_description.style.fontSize = (scale)+"vw";
-		}
-	}
-}
 
 function triggerFadeInAnimation(item)
 {
@@ -263,8 +162,6 @@ function projects()
 	
 	description.appendChild(card_content);
 	
-	setCardDescriptionFontSize();
-	
 	triggerFadeInAnimation(title);
 	triggerFadeInAnimation(description);
 }
@@ -284,17 +181,3 @@ function contacts()
 	triggerFadeInAnimation(title);
 	triggerFadeInAnimation(description);
 }
-
-//if the windows is resized
-window.onresize = () => {
-	setHeaderFooterFontSize();
-	setNavBarFontSize();
-	setContentFontSize();
-	setCardDescriptionFontSize();
-};
-
-//initial call
-setHeaderFooterFontSize();
-setNavBarFontSize();
-setContentFontSize();
-setCardDescriptionFontSize();
